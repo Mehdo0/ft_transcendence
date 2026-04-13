5341e15e7309db3c44b3cb1b7c8c4eeeb91d9976
Mehdo0 <mmouaffa@student@42lausanne.ch>
2026-04-13 13:22:44 +0200

    setup my own machine with npm

---
+++ frontend/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.BK9c3AqU.js","_app/immutable/chunks/CK-Cv2UY.js","_app/immutable/chunks/BZMfIxyW.js","_app/immutable/chunks/C-_02kA4.js","_app/immutable/chunks/ClfMKyPi.js","_app/immutable/chunks/DRsxHkMd.js"];
+export const stylesheets = [];
+export const fonts = [];
---
+++ frontend/.svelte-kit/output/server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2.BKKm0Eba.js","_app/immutable/chunks/CK-Cv2UY.js","_app/immutable/chunks/BZMfIxyW.js","_app/immutable/chunks/C-_02kA4.js","_app/immutable/chunks/CfpKsqlZ.js","_app/immutable/chunks/CbR6Gvdm.js","_app/immutable/chunks/DM53kcbL.js"];
+export const stylesheets = ["_app/immutable/assets/2.BPZTeZZZ.css"];
+export const fonts = [];
