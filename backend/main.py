30062e224a22ab2220b415ff6b74daa5fef4bd4f
Kim <thkm.grd@gmail.com>
2026-04-15 10:56:21 +0200

    add docker setup

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from fastapi import SQL
-
 app = FastAPI()
 
 app.mount("/", StaticFiles(directory="frontend_dist", html=True), name="frontend")
 
 @app.get("/api/user/{user_id}/stats")
-async def get_data():
+async def get_data(user_id: str):
+    return {}
 
 @app.exception_handler(404)
 async def not_found_exception_handler(request, exc):
