7720c40712bc56fb5be0d5c20a68be762888c08a
Nils <nils@example.com>
2026-04-21 16:42:12 +0200

    migration to SveltKit

--- frontend/src/routes/game/in-game/+page.svelte
+++ frontend/src/routes/game/in-game/+page.svelte
@@ -11,7 +11,7 @@
 	});
 
 	function resize() {
-		const dpr = window.devicePixelRatio || 1;
+		const dpr = window.devicePixaelRatio || 1;
 
 		canvas.width = window.innerWidth * dpr;
 		canvas.height = window.innerHeight * dpr;
@@ -25,38 +25,40 @@
 
 <svelte:window onresize={resize} />
 
-<canvas
-	bind:this={canvas}
-	onpointerdown={(e) => {
-		coords = { x: e.offsetX, y: e.offsetY };
+<div class="square">
 
-		context.fillStyle = color;
-		context.beginPath();
-		context.arc(coords.x, coords.y, size / 2, 0, 2 * Math.PI);
-		context.fill();
-	}}
-	onpointerleave={() => {
-		coords = null;
-	}}
-	onpointermove={(e) => {
-		const previous = coords;
+	<canvas
+		bind:this={canvas}
+		onpointerdown={(e) => {
+			coords = { x: e.offsetX, y: e.offsetY };
 
-		coords = { x: e.offsetX, y: e.offsetY };
-
-		if (e.buttons === 1) {
-			e.preventDefault();
-
-			context.strokeStyle = color;
-			context.lineWidth = size;
-			context.lineCap = 'round';
+			context.fillStyle = color;
 			context.beginPath();
-			context.moveTo(previous.x, previous.y);
-			context.lineTo(coords.x, coords.y);
-			context.stroke();
-		}
-	}}
-></canvas>
+			context.arc(coords.x, coords.y, size / 2, 0, 2 * Math.PI);
+			context.fill();
+		}}
+		onpointerleave={() => {
+			coords = null;
+		}}
+		onpointermove={(e) => {
+			const previous = coords;
 
+			coords = { x: e.offsetX, y: e.offsetY };
+
+			if (e.buttons === 1) {
+				e.preventDefault();
+
+				context.strokeStyle = color;
+				context.lineWidth = size;
+				context.lineCap = 'round';
+				context.beginPath();
+				context.moveTo(previous.x, previous.y);
+				context.lineTo(coords.x, coords.y);
+				context.stroke();
+			}
+		}}
+	></canvas>
+</div>
 {#if coords}
 	<div
 		class="preview"
@@ -65,6 +67,14 @@
 {/if}
 
 <style>
+	.square {
+		width: 90vw;
+		aspect-ratio: 1;
+		box-sizing: border-box;
+		border: 2px solid black;
+		position: relative;
+
+	}
 	canvas {
 		position: absolute;
 		left: 0;
@@ -86,4 +96,3 @@
 		pointer-events: none;
 	}
 </style>
-
