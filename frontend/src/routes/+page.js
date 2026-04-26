a0a35dd0679897c2083c5548f26ced0accc381cc
Lucien Faure <faurelucien@proton.me>
2026-04-26 16:56:00 +0200

    normalize path in docker-compose, add .dockerignore to skip `sending build context to docker daemon`, started work on simple api call to backend

---
+++ frontend/src/routes/+page.js
@@ -0,0 +1,8 @@
+// export async function load({ fetch }) {
+//   const res = await fetch("http://trsc_back:8000/");
+//   const data = await res.json();
+
+//   return {
+//     message: data.message
+//   };
+// }
