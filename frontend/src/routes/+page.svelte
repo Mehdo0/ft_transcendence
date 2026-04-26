1039f255ccfe0b7ab377fb278f9df9cb6f6b6d11
Lucien Faure <faurelucien@proton.me>
2026-04-26 14:43:23 +0200

    page links to ingame and lobby

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<body>
+  <a href="/game/in-game">Start game</a>
+  <div></div>
+  <a href="/game/lobby">Go to lobby</a>
+</body>
