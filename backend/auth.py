73d8b54056161d8d7fb34659977357aa5e896802
Mehdo0 <mmouaffa@student@42lausanne.ch>
2026-04-30 16:19:27 +0200

    database fixed , creation of Oauth2 and token verification , starting the matchmaking and elo configuration

---
+++ backend/auth.py
@@ -0,0 +1,136 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from backend.data import User, UserInDB, Token, TokenData
+from backend.database import add_user, DB_NAME
+import sqlite3
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
+def get_user(username: str) -> User | None:
+    # Connects to DB, fetches the user by ID, and returns the row.
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row  # factorise le resultat en dictionnaire
+    cursor = conn.cursor()
+    # le '?' est une protection contre les attaques par injection SQL
+    cursor.execute(
+        "SELECT username FROM users WHERE username = ?", (username,)
+    )
+    row = cursor.fetchone()
+    # et ca degage
+    conn.close()
+    if row is None:
+        return None # User not found
+    return UserInDB(username=row["username"], hashed_password=row["password"])
+
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        verify_password(password, DUMMY_HASH) #preventing timing attacks
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> User:
+    return current_user
+
+@app.post("/api/register/")
+async def registering(username: str, password: str):
+    username_test = get_user(username)
+    if username_test:
+        raise HTTPException(status_code=406, detail="Username already used")
+    add_user(username, password)
+    if get_user(username):
+        return {"user_created":username}
+    else:
+        raise HTTPException(status_code=500, detail="Error while adding user")
