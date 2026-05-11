06e149acdd99cbb9bf2f9b3cc6e7610d5b99521f
Nils <nils@example.com>
2026-05-11 15:42:18 +0200

    started lobby

--- frontend/src/routes/game/lobby/+page.svelte
+++ frontend/src/routes/game/lobby/+page.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+    function connect() {
+        console.log("Joining lobby...");
+        const ws = new WebSocket('wss://localhost/ws/');
+        
+    }
+</script>
+
 <h1>Lobby</h1>
+<button onclick={connect}>Join lobby</button>
