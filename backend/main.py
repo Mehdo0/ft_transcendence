64bda2c3c4edc1748f49dc6e80c85dc359e725b3
Lucien Faure <faurelucien@proton.me>
2026-05-11 16:28:23 +0200

    normalized routes in backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.websocket("/ws")
+@app.websocket("/ws/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("websocket connected")
