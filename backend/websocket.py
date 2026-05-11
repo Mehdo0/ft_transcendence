e18cff3a386691c13b97610e666b7cc77d8bcc69
Nils <nils@example.com>
2026-05-11 17:51:49 +0200

    websocket

--- backend/websocket.py
+++ backend/websocket.py
@@ -1,5 +1,5 @@
 from fastapi import WebSocket, WebSocketDisconnect
-from backend.global_var import app
+from backend.global_var import app, MATCHMAKING_QUEUE
 
 
 @app.websocket("/ws")
@@ -7,6 +7,16 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
     try:
-        while (true):
-            data = await.websocket
+        while True:
+            data = await websocket.receive_json()
+            type = data.get("type")
+
+            if type == "find_player":
+                MATCHMAKING_QUEUE["TWO_PLAYER_AI"].append(data.get("username"))
+
+            else:
+                await websocket.send_json({"event": "error", "msg": "unknown type"})
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        
 
