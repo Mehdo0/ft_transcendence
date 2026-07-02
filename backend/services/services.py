0b8592a93288b91c3462cf896ca86cd1bc7fdf13
Mohamed Mouaffak <mmouaffa@c3r4s1.42lausanne.ch>
2026-07-02 16:14:22 +0200

    api/user/me route is now off and replaced by api/session/ that does the same without raising an http error

--- backend/services/services.py
+++ backend/services/services.py
@@ -123,25 +123,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(cookie_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except InvalidTokenError:
-        raise credentials_exception
-    user = get_user(username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
 def get_user_from_ws_token(token: str) -> User:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
